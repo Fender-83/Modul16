66a8ffa5ebdf1a5bc685145fd8996ab9a229d11a
Name <fenderpugach@gmail.com>
2023-04-21 09:46:44 +0700

    Добавьте файлы проекта.

---
+++ 16modul.Tests.cs
@@ -0,0 +1,64 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace _16modul
+{
+    internal class _16modul
+    {
+        public class Calculator
+        { 
+            public int Additional(int a, int b)
+            {
+                return a + b;
+            }
+            [Test]
+            public void Additional_ReturnCorrectValue()
+            {
+                Calculator calculator = new Calculator();
+                Assert.That(calculator.Additional(30, 10) == 40);
+            }
+
+
+            public int Subtraction(int a, int b)
+            {
+                return a - b;
+            }
+            [Test]
+            public void Subtraction_ReturnCorrectValue()
+            {
+                Calculator calculator = new Calculator();
+                Assert.That(calculator.Subtraction(30, 10) == 20);
+            }
+
+
+            public int Miltiplication(int a, int b)
+            {
+                return a * b;
+            }
+            [Test]
+            public void Miltiplication_ReturnCorrectValue()
+            {
+                Calculator calculator = new Calculator();
+                Assert.That(calculator.Miltiplication(30, 10) == 300);
+            }
+
+
+            public int Division(int a, int b)
+            {
+                return a / b;
+            }
+            [Test]
+            public void Division_ReturnCorrectValue()
+            {
+                var calculator = new Calculator();
+                Assert.That(calculator.Division(25, 5) == 5);
+            }
+            [Test]
+            public void Division_ByZero()
+            {
+                var calculator = new Calculator();
+                Assert.Throws<DivideByZeroException>(() => calculator.Division(220, 0));
+            }
+        }
+    }
+}
